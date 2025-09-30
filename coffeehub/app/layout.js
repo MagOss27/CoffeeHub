b555980657ede9a127435d2c94e6037dd1aa3daa
MagOss27 <magnusoss98@gmail.com>
2025-09-29 21:57:06 -0300

    feat: inicializar projeto CoffeeHub com Tailwind v4 e header responsivo

---
+++ coffeehub/app/layout.js
@@ -0,0 +1,18 @@
+import { Jost } from "next/font/google";
+import './globals.css'
+
+// Configuração da fonte
+const jost = Jost({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Coffee Shop",
+  description: "Coffee Shop",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={jost.className}>{children}</body>
+    </html>
+  );
+}
