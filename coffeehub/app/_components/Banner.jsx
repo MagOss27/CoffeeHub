f424085c95a0fd6c6f98e1c134435653f9c43637
MagOss27 <magnusoss98@gmail.com>
2025-10-01 23:24:00 -0300

    feat: melhoria nos cards e na tela no Banner

--- coffeehub/app/_components/Banner.jsx
+++ coffeehub/app/_components/Banner.jsx
@@ -15,7 +15,7 @@ import { Button } from "@/components/ui/button";
 const Banner = () => {
     return (
         <>
-            <div className="container mx-auto max-w-7xl p-6 lg:px-8 ">
+            <div className="bg-gray-100 mx-auto p-6 lg:px-8 ">
                 <Swiper
                     spaceBetween={30}
                     centeredSlides={true}
@@ -30,33 +30,102 @@ const Banner = () => {
                     modules={[Autoplay, Pagination]}
                     className="mySwiper"
                 >
-                    <SwiperSlide>
-                        <div className="container">
+                    <SwiperSlide className="cursor-pointer">
+                        <div className="container md:pt-20 md:pb-10">
                             {/* ***************** */}
-                            <div className="grid grid-cols-2">
+                            <div className="grid grid-cols-2 w-full max-w-5xl mx-auto">
                                 {/* ***************** */}
-                                <div className="">
+                                <div className="relative">
                                     {/* ***************** */}
-                                    <div className=""></div>
-                                    {/* ***************** */}
-                                    <h1>
+                                    <h1 className="text-8xl font-bold py-4">
                                         Alowishus Delicious Coffee
-                                        <Image src="/images/cafe.png" width={80} height={80} alt="Café" />
                                     </h1>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati quo veritatis laudantium a esse nihil assumenda, porro vero dicta.</p>
+                                    <div className="absolute top-56 mt-1 right-30">
+                                        <Image src="/images/cafe.png" width={80} height={80} alt="Café" />
+                                    </div>
+                                    <p className="py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati quo veritatis laudantium a esse nihil assumenda, porro vero dicta.</p>
                                     {/* ***************** */}
                                     <div className="flex gap-4 py-6">
-                                        <Button   variant="black" className= "p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal ">Download App</Button>
-                                        <Button className="p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal" variant="outline">Coffee Hub</Button>
+                                        <Button variant="black" className="p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide">Download App</Button>
+                                        <Button className="p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide" variant="outline">Coffee Hub</Button>
                                     </div>
                                     {/* ***************** */}
                                 </div>
                                 {/* ***************** */}
-                                <div className="">apfaofpm</div>
+                                <div className="relative">
+                                    <Image src="/images/hero1.png" width={600} height={600} alt="Café" />
+                                    <div className="absolute top-5 right-5">
+                                        <Image src="/images/cafe.png" width={120} height={120} alt="Café" />
+                                    </div>
+                                </div>
+                            </div>
+                            {/* ***************** */}
+                        </div>
+                    </SwiperSlide >
+                    {/*******************/}
+                    <SwiperSlide className="cursor-pointer">
+                        <div className="container md:pt-20 md:pb-10">
+                            {/* ***************** */}
+                            <div className="grid grid-cols-2 w-full max-w-5xl mx-auto">
+                                {/* ***************** */}
+                                <div className="relative">
+                                    {/* ***************** */}
+                                    <h1 className="text-8xl font-bold py-4">
+                                        Alowishus Delicious Coffee
+                                    </h1>
+                                    <div className="absolute top-56 mt-1 right-30">
+                                        <Image src="/images/cafe.png" width={80} height={80} alt="Café" />
+                                    </div>
+                                    <p className="py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati quo veritatis laudantium a esse nihil assumenda, porro vero dicta.</p>
+                                    {/* ***************** */}
+                                    <div className="flex gap-4 py-6">
+                                        <Button variant="black" className="p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide">Download App</Button>
+                                        <Button className="p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide" variant="outline">Coffee Hub</Button>
+                                    </div>
+                                    {/* ***************** */}
+                                </div>
+                                {/* ***************** */}
+                                <div className="relative">
+                                    <Image src="/images/coffee-mid.png" width={600} height={600} alt="Café" />
+                                </div>
                             </div>
                             {/* ***************** */}
                         </div>
                     </SwiperSlide>
+                    {/*******************/}
+                    <SwiperSlide className="cursor-pointer">
+                        <div className="container md:pt-20 md:pb-10">
+                            {/* ***************** */}
+                            <div className="grid grid-cols-2 w-full max-w-5xl mx-auto">
+                                {/* ***************** */}
+                                <div className="relative">
+                                    {/* ***************** */}
+                                    <h1 className="text-8xl font-bold py-4">
+                                        Alowishus Delicious Coffee
+                                    </h1>
+                                    <div className="absolute top-56 mt-1 right-30">
+                                        <Image src="/images/cafe.png" width={80} height={80} alt="Café" />
+                                    </div>
+                                    <p className="py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati quo veritatis laudantium a esse nihil assumenda, porro vero dicta.</p>
+                                    {/* ***************** */}
+                                    <div className="flex gap-4 py-6">
+                                        <Button variant="black" className="p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide">Download App</Button>
+                                        <Button className="p-7 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide" variant="outline">Coffee Hub</Button>
+                                    </div>
+                                    {/* ***************** */}
+                                </div>
+                                {/* ***************** */}
+                                <div className="relative">
+                                    <Image src="/images/alowishus-coffee.png" width={1000} height={1000} alt="Café" />
+                               <div className="absolute top-5 right-5">
+                                        <Image src="/images/cafe.png" width={120} height={120} alt="Café" />
+                                    </div>
+                                </div>
+                            </div>
+                            {/* ***************** */}
+                        </div>
+                    </SwiperSlide>
+                    {/*******************/}
                 </Swiper>
             </div>
         </>
--- coffeehub/app/_components/Header.jsx
+++ coffeehub/app/_components/Header.jsx
@@ -49,135 +49,137 @@ const Header = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-   <header className="bg-white container">
-<nav aria-label="Global" className="mx-auto flex max-w-7xl  items-center justify-between p-6 lg:px-8">
-    {/* <header className="text-white">
+        <div className='bg-gray-100 '>
+            <header className="container">
+                <nav aria-label="Global" className="mx-auto flex max-w-7xl  items-center justify-between p-6 lg:px-8">
+                    {/* <header className="text-white">
 <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"> */}
 
 
-                {/* ************************** */}
-                <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <Image
-                            src="/images/logo.png" alt="Logo CoffeeHub" width={120} height={40}
-                        />
-                    </a>
-                </div>
-                {/* ************************** */}
-                <div className="flex lg:hidden">
-                    <button
-                        type="button"
-                        onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon aria-hidden="true" className="size-6" />
-                    </button>
-                </div>
-                {/* ************************** */}
-                <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-                    <a href="#" className="text-lg font-medium text-black">
-                        Cafe Menu
-                    </a>
-                    <a href="#" className="text-lg font-medium text-black">
-                        About Us
-                    </a>
-                    <a href="#" className="text-lg font-medium text-black">
-                        Find Us
-                    </a>
-                    <a href="#" className="text-lg font-medium text-black">
-                        Alowishus Catering
-                    </a>
-                </PopoverGroup>
-                {/* ************************** */}
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-5">
-                    <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                        <ShoppingBagIcon
-                            width={35}
-                            className='hover:cursor-pointer bg-gray-50 hover:bg-gray-100  p-2 rounded-full  shadow-xl shadow-gray-300  hover:shadow-xl hover:scale-105 transition-all'
-                        />
-                    </a>
-                    <Button className="bg-gray-900 text-white p-3 rounded-md shadow-lg hover:scale-105 transition-all">Buy Gift Vouchers</Button>
-                </div>
-                {/* ************************** */}
-            </nav>
-            {/* ************************** */}
-            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                <div className="fixed inset-0 z-50" />
-                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
-                    <div className="flex items-center justify-between">
+                    {/* ************************** */}
+                    <div className="flex lg:flex-1">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
-                            <img
-                                alt=""
-                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                                className="h-8 w-auto"
+                            <Image
+                                src="/images/logo.png" alt="Logo CoffeeHub" width={120} height={40}
                             />
                         </a>
+                    </div>
+                    {/* ************************** */}
+                    <div className="flex lg:hidden">
                         <button
                             type="button"
-                            onClick={() => setMobileMenuOpen(false)}
-                            className="-m-2.5 rounded-md p-2.5 text-gray-400"
+                            onClick={() => setMobileMenuOpen(true)}
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
                         >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon aria-hidden="true" className="size-6" />
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon aria-hidden="true" className="size-6" />
                         </button>
                     </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-white/10">
-                            <div className="space-y-2 py-6">
-                                <Disclosure as="div" className="-mx-3">
-                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-white hover:bg-white/5">
-                                        Product
-                                        <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
-                                    </DisclosureButton>
-                                    <DisclosurePanel className="mt-2 space-y-2">
-                                        {[...products, ...callsToAction].map((item) => (
-                                            <DisclosureButton
-                                                key={item.name}
-                                                as="a"
-                                                href={item.href}
-                                                className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-white hover:bg-white/5"
-                                            >
-                                                {item.name}
-                                            </DisclosureButton>
-                                        ))}
-                                    </DisclosurePanel>
-                                </Disclosure>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
-                                >
-                                    Features
-                                </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
-                                >
-                                    Marketplace
-                                </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
-                                >
-                                    Company
-                                </a>
-                            </div>
-                            <div className="py-6">
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
-                                >
-                                    Log in
-                                </a>
+                    {/* ************************** */}
+                    <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+                        <a href="#" className="text-lg font-medium text-black">
+                            Cafe Menu
+                        </a>
+                        <a href="#" className="text-lg font-medium text-black">
+                            About Us
+                        </a>
+                        <a href="#" className="text-lg font-medium text-black">
+                            Find Us
+                        </a>
+                        <a href="#" className="text-lg font-medium text-black">
+                            Alowishus Catering
+                        </a>
+                    </PopoverGroup>
+                    {/* ************************** */}
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-5">
+                        <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                            <ShoppingBagIcon
+                                width={35}
+                                className='hover:cursor-pointer bg-gray-50 hover:bg-gray-100  p-2 rounded-full  shadow-xl shadow-gray-300  hover:shadow-xl hover:scale-105 transition-all'
+                            />
+                        </a>
+                        <Button className="bg-gray-900 text-white p-3 rounded-md shadow-lg hover:scale-105 transition-all">Buy Gift Vouchers</Button>
+                    </div>
+                    {/* ************************** */}
+                </nav>
+                {/* ************************** */}
+                <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                    <div className="fixed inset-0 z-50" />
+                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Your Company</span>
+                                <img
+                                    alt=""
+                                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                                    className="h-8 w-auto"
+                                />
+                            </a>
+                            <button
+                                type="button"
+                                onClick={() => setMobileMenuOpen(false)}
+                                className="-m-2.5 rounded-md p-2.5 text-gray-400"
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon aria-hidden="true" className="size-6" />
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-white/10">
+                                <div className="space-y-2 py-6">
+                                    <Disclosure as="div" className="-mx-3">
+                                        <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-white hover:bg-white/5">
+                                            Product
+                                            <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
+                                        </DisclosureButton>
+                                        <DisclosurePanel className="mt-2 space-y-2">
+                                            {[...products, ...callsToAction].map((item) => (
+                                                <DisclosureButton
+                                                    key={item.name}
+                                                    as="a"
+                                                    href={item.href}
+                                                    className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-white hover:bg-white/5"
+                                                >
+                                                    {item.name}
+                                                </DisclosureButton>
+                                            ))}
+                                        </DisclosurePanel>
+                                    </Disclosure>
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                                    >
+                                        Features
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                                    >
+                                        Marketplace
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                                    >
+                                        Company
+                                    </a>
+                                </div>
+                                <div className="py-6">
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
+                                    >
+                                        Log in
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </DialogPanel>
-            </Dialog>
-            {/* ************************** */}
-        </header>
+                    </DialogPanel>
+                </Dialog>
+                {/* ************************** */}
+            </header>
+        </div>
     )
 }
 {/* ************************** */ }
